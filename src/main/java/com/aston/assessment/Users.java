b27f81f490f9ff1280e3900cb5cdda8cf75da145
Max Michael Mendonca <mendoncamax123@gmail.com>
2024-08-09 15:54:50 +0100

    changes

---
+++ src/main/java/com/aston/assessment/Users.java
@@ -0,0 +1,38 @@
+package com.aston.assessment;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="Users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRoles role = UserRoles.NOT_SPECIFIED;
+
+
+
+
+
+
+}
